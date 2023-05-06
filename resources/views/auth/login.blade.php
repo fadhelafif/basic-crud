3690cdb9368399429827f028110e5f032509538e
Fadhel Afif <deel.mr0@gmail.com>
2023-05-07 01:09:56 +0700

    add guest layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,45 +1,32 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700&amp;display=swap">
-
-        <title>Talentlytica</title>
-
-        @vite(['resources/scss/app.scss', 'resources/js/app.js'])
-    </head>
-    <body class="bg-gray">
-        <section class="">
-            <div class="container pt-5">
-                <h2 class="text-center mb-5">Log In</h2>
-                <form class="widget" method="POST" action="{{ route('login') }}">
-                    @csrf
-                    <div class="mb-3">
-                        <label class="form-label" for="email">Email</label>
-                        <x-text-input id="email" class="form-control item" type="email" name="email" :value="old('email')" />
-                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label" for="password">Password</label>
-                        <x-text-input id="password" class="form-control" type="password" name="password" :value="old('password')" />
-                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                    </div>
-                    <div class="mb-3">
-                        <div class="form-check">
-                            <input
-                                id="checkbox"
-                                class="form-check-input"
-                                type="checkbox"
-                            /><label class="form-check-label" for="checkbox">Remember me</label>
-                        </div>
-                    </div>
-                    <button class="btn btn-primary" type="submit">Log In</button>
-                </form>
-                <div class="text-center mt-5">
-                    <p>Don't have an account yet? <a href="{{ route('register') }}">Register</a></p>
-                </div>
+<x-layouts.guest>
+    <x-slot:title>
+        Log In
+    </x-slot>
+    <h2 class="text-center mb-5">Log In</h2>
+    <form class="widget" method="POST" action="{{ route('login') }}">
+        @csrf
+        <div class="mb-3">
+            <label class="form-label" for="email">Email</label>
+            <x-text-input id="email" class="form-control item" type="email" name="email" :value="old('email')" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="password">Password</label>
+            <x-text-input id="password" class="form-control" type="password" name="password" :value="old('password')" />
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+        <div class="mb-3">
+            <div class="form-check">
+                <input
+                    id="checkbox"
+                    class="form-check-input"
+                    type="checkbox"
+                /><label class="form-check-label" for="checkbox">Remember me</label>
             </div>
-        </section>
-    </body>
-</html>
+        </div>
+        <button class="btn btn-primary" type="submit">Log In</button>
+    </form>
+    <div class="text-center mt-5">
+        <p>Don't have an account yet? <a href="{{ route('register') }}">Register</a></p>
+    </div>
+</x-layouts.guest>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,46 +1,33 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700&amp;display=swap">
+<x-layouts.guest>
+    <x-slot:title>
+        Register
+    </x-slot>
+    <h2 class="text-center mb-5">Register</h2>
+    <form class="widget" method="POST" action="{{ route('register') }}">
+        @csrf
+        <div class="mb-3">
+            <label class="form-label" for="name">Name</label>
+            <x-text-input id="name" type="text" name="name" :value="old('name')" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="email">Email</label>
+            <x-text-input id="email" type="email" name="email" :value="old('email')" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="password">Password</label>
+            <x-text-input id="password" type="password" name="password" :value="old('password')" />
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="password_confirmation">Confirm Password</label>
+            <x-text-input id="password_confirmation" type="password" name="password_confirmation" :value="old('password_confirmation')" />
+        </div>
 
-        <title>Talentlytica</title>
-
-        @vite(['resources/scss/app.scss', 'resources/js/app.js'])
-    </head>
-    <body class="bg-gray">
-        <section class="">
-            <div class="container pt-5">
-                <h2 class="text-center mb-5">Register</h2>
-                <form class="widget" method="POST" action="{{ route('register') }}">
-                    @csrf
-                    <div class="mb-3">
-                        <label class="form-label" for="name">Name</label>
-                        <x-text-input id="name" type="text" name="name" :value="old('name')" />
-                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label" for="email">Email</label>
-                        <x-text-input id="email" type="email" name="email" :value="old('email')" />
-                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label" for="password">Password</label>
-                        <x-text-input id="password" type="password" name="password" :value="old('password')" />
-                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label" for="password_confirmation">Confirm Password</label>
-                        <x-text-input id="password_confirmation" type="password" name="password_confirmation" :value="old('password_confirmation')" />
-                    </div>
-
-                    <button class="btn btn-primary" type="submit">Register</button>
-                </form>
-                <div class="text-center mt-5">
-                    <p>Already have an account? <a href="{{ route('login') }}">Sign in</a></p>
-                </div>
-            </div>
-        </section>
-    </body>
-</html>
+        <button class="btn btn-primary" type="submit">Register</button>
+    </form>
+    <div class="text-center mt-5">
+        <p>Already have an account? <a href="{{ route('login') }}">Sign in</a></p>
+    </div>
+</x-layouts.guest>
