153882f547dba494daf8bcbc6ada5784afa4bcf1
Fadhel Afif <deel.mr0@gmail.com>
2023-05-07 06:02:39 +0700

    add admin layout

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700&amp;display=swap">
-
-        <title>Talentlytica</title>
-
-        @vite(['resources/scss/app.scss', 'resources/js/app.js'])
-    </head>
-    <body class="bg-gray">
-        <div class="container pt-5">
-            <h2 class="text-center mb-5">Dashboard</h2>
-        </div>
-    </body>
-</html>
+<x-layouts.admin>
+    <x-slot:title>
+        Dashboard
+    </x-slot>
+    <div class="container pt-5">
+        <h2 class="text-center mb-5">Dashboard</h2>
+    </div>
+</x-layouts.admin>
---
+++ resources/views/components/layouts/admin.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700&amp;display=swap">
+
+        <title>{{ $title ?? 'Talentlytica' }}</title>
+
+        @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+    </head>
+    <body class="bg-gray">
+        <nav class="navbar navbar-light navbar-expand-lg bg-white clean-navbar shadow-sm">
+            <div class="container"><a class="navbar-brand logo" href="#">Talentlytica</a><button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div id="navcol-1" class="collapse navbar-collapse">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="{{ route('dashboard') }}">Dashboard</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('participant.index') }}">Participant</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('logout') }}">Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        {{ $slot }}
+    </body>
+</html>
