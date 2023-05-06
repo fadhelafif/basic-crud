4d9fd1d066ae0b7ff77f43357e2f6c0786352e65
Fadhel Afif <deel.mr0@gmail.com>
2023-05-07 00:42:19 +0700

    fix login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,13 +14,16 @@
             <div class="container pt-5">
                 <h2 class="text-center mb-5">Log In</h2>
                 <form class="widget" method="POST" action="{{ route('login') }}">
+                    @csrf
                     <div class="mb-3">
-                        <label class="form-label" for="email">Email</label
-                        ><input id="email" class="form-control item" type="email" />
+                        <label class="form-label" for="email">Email</label>
+                        <x-text-input id="email" class="form-control item" type="email" name="email" :value="old('email')" />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
                     </div>
                     <div class="mb-3">
-                        <label class="form-label" for="password">Password</label
-                        ><input id="password" class="form-control" type="password" />
+                        <label class="form-label" for="password">Password</label>
+                        <x-text-input id="password" class="form-control" type="password" name="password" :value="old('password')" />
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
                     </div>
                     <div class="mb-3">
                         <div class="form-check">
@@ -28,9 +31,7 @@
                                 id="checkbox"
                                 class="form-check-input"
                                 type="checkbox"
-                            /><label class="form-check-label" for="checkbox"
-                                >Remember me</label
-                            >
+                            /><label class="form-check-label" for="checkbox">Remember me</label>
                         </div>
                     </div>
                     <button class="btn btn-primary" type="submit">Log In</button>
