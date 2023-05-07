21361fbdc17b5ae0de1219bc3c385a501338faa9
Fadhel Afif <deel.mr0@gmail.com>
2023-05-07 11:25:23 +0700

    add link at brand

--- resources/views/components/layouts/admin.blade.php
+++ resources/views/components/layouts/admin.blade.php
@@ -11,7 +11,7 @@
     </head>
     <body class="bg-gray">
         <nav class="navbar navbar-light navbar-expand-lg bg-white clean-navbar shadow-sm">
-            <div class="container"><a class="navbar-brand logo" href="#">Talentlytica</a><button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="container"><a class="navbar-brand logo" href="{{ route('dashboard') }}">Talentlytica</a><button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
                 <div id="navcol-1" class="collapse navbar-collapse">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item"><a class="nav-link" href="{{ route('dashboard') }}">Dashboard</a></li>
