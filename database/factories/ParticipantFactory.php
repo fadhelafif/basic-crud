dde43e83b9cf9f6e53cbffb8cee935acc4b8bf9d
Fadhel Afif <deel.mr0@gmail.com>
2023-05-07 11:19:20 +0700

    add participant seeder with factory

---
+++ database/factories/ParticipantFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Participant>
+ */
+class ParticipantFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'author_id' => 1,
+            'name' => fake()->name(),
+            'email' => fake()
+                ->unique()
+                ->safeEmail(),
+            'x' => fake()->numberBetween(1, 33),
+            'y' => fake()->numberBetween(1, 23),
+            'z' => fake()->numberBetween(1, 18),
+            'w' => fake()->numberBetween(1, 13),
+        ];
+    }
+}
